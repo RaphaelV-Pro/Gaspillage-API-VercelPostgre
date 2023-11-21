e6c7df08537307bbbfbc5c5cdadf5b67ac3dc83f
RaphaelV-Pro <raphaelviennet.entreprise@gmail.com>
2023-11-21 23:44:39 +0100

    Added all user management

--- routes/users.router.js
+++ routes/users.router.js
@@ -4,10 +4,10 @@ const router = express.Router()
 const usersController = require('../controllers/users.controller')
 
 router.get("/", usersController.getAll)
-router.get("/:id", bookController.getById)
-router.post("/", bookController.create)
-router.put("/:id", bookController.updateById)
-router.delete("/:id", bookController.deleteById)
+router.get("/:id", usersController.getById)
+router.post("/", usersController.create)
+router.put("/:id", usersController.updateById)
+router.delete("/:id", usersController.deleteById)
 
 
 module.exports = router;
